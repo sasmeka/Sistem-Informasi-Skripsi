d0159bb93450c3a2238bccc03e55c66253d19451
sasmeka <verdysas@gmail.com>
2023-02-21 19:52:00 +0700

    refresh

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,8 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://sisri.trunojoyo.ac.id/';
+    // public string $baseURL = 'http://sisri.trunojoyo.ac.id/';
+    public string $baseURL = 'http://localhost/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
