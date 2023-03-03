57b76d9aaff6db9c148d3ac6f7a90d0588dcb75c
sasmeka <verdysas@gmail.com>
2023-03-03 22:12:52 +0700

    refresh

--- app/Controllers/Admin/Mahasiswa.php
+++ app/Controllers/Admin/Mahasiswa.php
@@ -134,39 +134,44 @@ class Mahasiswa extends BaseController
     }
     public function update_data_mhs()
     {
-        if (session()->get('ses_id') == '' || session()->get('ses_login') != 'admin') {
-            return redirect()->to('/');
-        }
+        // if (session()->get('ses_id') == '' || session()->get('ses_login') != 'admin') {
+        //     return redirect()->to('/');
+        // }
         set_time_limit(0);
         ini_set('max_execution_time', 0);
         ini_set('max_input_time', 0);
         $data1 = $this->api->get_meta_api("https://api.trunojoyo.ac.id:8212/siakad/v1/mahasiswa?page=1&take=1000");
-        // $total_page = intval($data1->pageCount);
-        // function add_data($page, $a)
-        // {
-        //     $array_data = [];
-        //     $data = $a->api->get_data_api("https://api.trunojoyo.ac.id:8212/siakad/v1/mahasiswa?page=$page&take=1000");
-        //     foreach ($data as $key) {
-        //         $nama = '"' . $key->nama . '"';
-        //         $cek_data = $a->db->query("SELECT count(nim) as jumlah from tb_mahasiswa where nim='$key->nim'")->getResult()[0]->jumlah;
-        //         if ($cek_data == 0) {
-        //             array_push($array_data, "INSERT INTO tb_mahasiswa (nim, nama, jk, idunit, idperiode, email, `page`) VALUES ('$key->nim', " . $nama . ", '$key->jk','$key->idunit','$key->idperiode','$key->email','$page')");
-        //         } else {
-        //             array_push($array_data, "UPDATE tb_mahasiswa SET nama=" . $nama . ", jk='$key->jk', idunit='$key->idunit', idperiode='$key->idperiode', email='$key->email',`page`='$page' WHERE nim='$key->nim')");
-        //         }
-        //     }
-        //     return $array_data;
-        // }
-        // $r = [];
-        // for ($i = 1; $i <= $total_page; $i++) {
-        //     $result = add_data($i, $this);
-        //     $r = array_merge($r, $result);
-        // }
+        $total_page = intval($data1->pageCount);
+        $this->db->query("TRUNCATE TABLE tb_mahasiswa");
+        function add_data($page, $a)
+        {
+            $array_data = [];
+            $data = $a->api->get_data_api("https://api.trunojoyo.ac.id:8212/siakad/v1/mahasiswa?page=$page&take=1000");
+            foreach ($data as $key) {
+                // if(){
+                $nama = '"' . $key->nama . '"';
+                // $cek_data = $a->db->query("SELECT count(nim) as jumlah from tb_mahasiswa where nim='$key->nim'")->getResult()[0]->jumlah;
+                // if ($cek_data == 0) {
+                array_push($array_data, "INSERT INTO tb_mahasiswa (nim, nama, jk, idunit, idperiode, email, `page`) VALUES ('$key->nim', " . $nama . ", '$key->jk','$key->idunit','$key->idperiode','$key->email','$page');");
+                // } else {
+                // array_push($array_data, "UPDATE tb_mahasiswa SET nama=" . $nama . ", jk='$key->jk', idunit='$key->idunit', idperiode='$key->idperiode', email='$key->email',`page`='$page' WHERE nim='$key->nim')");
+                // }
+                // }
+            }
+            return $array_data;
+        }
+        $r = [];
+        for ($i = 1; $i <= $total_page; $i++) {
+            $result = add_data($i, $this);
+            $r = array_merge($r, $result);
+        }
+        $rr = implode(' ', $r);
+        // $this->db->query("$rr");
         // for ($i = 0; $i < count($r); $i++) {
         //     $this->db->query("$r[$i]");
         // }
         // $this->db->query("INSERT INTO tb_log (user,`action`,`log`,date_time) VALUES ('" . session()->get('ses_id') . "','insert or update','Update Data Mahasiswa',now())");
-        return redirect()->to('/data_mahasiswa');
+        // return redirect()->to('/data_mahasiswa');
 
         // function add_data($page, $a,$unit,$periode)
         // {
@@ -185,6 +190,6 @@ class Mahasiswa extends BaseController
         //     $result = add_data($i, $this,'P-31','20171');
         //     $r = array_merge($r, $result);
         // }
-        // var_dump($r);
+        echo $rr;
     }
 }
